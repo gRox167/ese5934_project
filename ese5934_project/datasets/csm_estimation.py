b36e07cbd5d25013b4a262b23aff29edceedcec6
Chunxu Guo <525563615@qq.com>
2024-02-12 14:07:20 -0600

    bart installation and import bart

--- ese5934_project/datasets/csm_estimation.py
+++ ese5934_project/datasets/csm_estimation.py
@@ -1,8 +1,9 @@
-import bart
 from fastmri import tensor_to_complex_np
 
+from ese5934_project.bart import bart
 
-def espirit_csm_estimation(kspace, num_low_freq)
+
+def espirit_csm_estimation(kspace, num_low_freqs):
     kspace = kspace.permute(1, 2, 0, 3).unsqueeze(0)
     kspace = tensor_to_complex_np(kspace)
 
