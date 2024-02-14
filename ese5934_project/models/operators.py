e63eee3467ef1afd884d4f83389eb497af9331c4
Chunxu Guo <chunxuguo@chunxus-mbp.wusm-wifi.wucon.wustl.edu>
2024-02-14 17:15:34 -0600

    update dependency

--- ese5934_project/models/operators.py
+++ ese5934_project/models/operators.py
@@ -1,64 +1,91 @@
-import torch 
+import torch
 from einops import rearrange, repeat
 from torch import nn
 
-    
-class S(nn.module):
+
+class C(nn.module):
     """
     Sensitivity forward operator to do SENS expansion
     """
+
     def __init__(self):
         super(self).__init__()
+
     def forward(self, x, csm):
-        return x*csm
-    
-class S_adj(nn.module):
+        return x * csm
+
+
+class C_adj(nn.module):
     """
     Sensitivity adjoint operator to do SENS expansion
     """
+
     def __init__(self):
         super(self).__init__()
+
     def forward(self, x, csm):
-        return (x*csm.conj()).sum(dim=1)
+        return (x * csm.conj()).sum(dim=1)
 
 
 class F(nn.module):
     """
     Fourier forward operator
     """
+
     def __init__(self):
         super().__init__()
+
     def forward(self, x):
         x = torch.fft.ifftshift(x, dim=(-2, -1))
-        x = torch.fft.fftn(x, dim=(-2, -1), norm='ortho')
+        x = torch.fft.fftn(x, dim=(-2, -1), norm="ortho")
         # fftshift
         x = torch.fft.fftshift(x, dim=(-2, -1))
         # x = rearrange(x, 'b c h w -> b (h w) c')
         return x
-    
+
+
 class F_adj(nn.module):
     """
     Fourier adjoint operator
     """
+
     def __init__(self):
         super().__init__()
+
     def forward(self, x):
         x = torch.fft.ifftshift(x, dim=(-2, -1))
-        x = torch.fft.ifftn(x, dim=(-2, -1), norm='ortho')
+        x = torch.fft.ifftn(x, dim=(-2, -1), norm="ortho")
         # fftshift
         x = torch.fft.fftshift(x, dim=(-2, -1))
         # x = rearrange(x, 'b (h w) c -> b c h w', h=self.n, w=self.m)
         return x
 
+
+class M(nn.module):
+    """
+    Masking forward operator
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, mask):
+        return x * mask
+
+
 class MRForwardModel(nn.module):
     """
     MR forward model to do SENS expansion and Fourier transform
     """
+
     def __init__(self):
         super(self).__init__()
-        self.S = S()
+        self.C = C()
         self.F = F()
-    def forward(self, x, csm):
-        x = self.S(x, csm)
+        self.M = M()
+
+    def forward(self, x, csm, mask):
+        x = self.C(x, csm)
         x = self.F(x)
+        x = self.M(x, mask)
         return x
